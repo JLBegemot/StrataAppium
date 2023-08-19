128138f89d493e4418883d6029f62f5cb42a3620
lynevivan <ivan.lunev@indriver.com>
2023-08-19 23:36:09 +0300

    init commit

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver<MobileElement> driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "org.wikipedia");
+        caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "org.wikipedia.main.MainActivity");
+        caps.setCapability(MobileCapabilityType.APP, "");
+        //Сюда надо прописать полный путь до APK файла, лежащего в папке APKs
+
+        URL appiumURL = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AppiumDriver<MobileElement>(appiumURL, caps);
+
+    }
+
+    @After
+    public void  tearDown() {
+        driver.quit();
+    }
+}
